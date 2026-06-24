e50f5e8d4d7ee08ba3d6381a4d094872ea8f7fa4
wdjacobo <113604892+wdjacobo@users.noreply.github.com>
2026-06-24 18:35:28 +0200

    Add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
-    site: 'https://wdjacobo-astro-tutorial.netlify.app'
+  site: 'https://wdjacobo-astro-tutorial.netlify.app',
+  integrations: [preact()]
 });
